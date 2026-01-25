eca1655ae9b15e99332f894fcfd7e9d3fee6f752
Santio <admin@santio.me>
2026-01-25 05:09:31 -0500

    fix: update ip data csv link

--- src/download.rs
+++ src/download.rs
@@ -8,7 +8,7 @@ use std::io::Write;
 use std::path::PathBuf;
 use tokio::fs;
 
-const DOWNLOAD_URL: &'static str = "https://download.db-ip.com/free/dbip-city-lite-2025-11.csv.gz";
+const DOWNLOAD_URL: &'static str = "https://download.db-ip.com/free/dbip-city-lite-2026-01.csv.gz";
 
 pub(crate) async fn start() -> anyhow::Result<PathBuf> {
     print!("📦  Downloading and compiling trie... ");
